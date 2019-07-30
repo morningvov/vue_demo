c73c45511bc588f7024e806b6da619c1625e8277
momo_glb <momo_glb@momo.com>
2019-07-30 23:44:28 +0800

    Vue-mintUI demo(button)

---
+++ src_ajax_users/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <search/>
+      <UsersMain/>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Main from './components/Main'
+  import Search from './components/Search'
+  export default {
+    components:{
+      UsersMain:Main,
+      Search
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
